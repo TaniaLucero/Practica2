4a629ae13dbe49c74375cdcab73dba1661745596
Tania Lucero <tanialucerorangel@gmail.com>
2020-03-12 21:41:30 -0600

    Initial commit

---
+++ app/src/main/java/mx/edu/tesoem/isc/tlgr/practica2/aleatorio.java
@@ -0,0 +1,18 @@
+package mx.edu.tesoem.isc.tlgr.practica2;
+
+public class aleatorio {
+    int Imagen;
+    String aleatorio;
+    public aleatorio(int Imagen,String aleatorio){
+        this.Imagen = Imagen;
+        this.aleatorio = aleatorio;
+    }
+
+    public int getImagen() {
+        return Imagen;
+    }
+
+    public String getAleatorio() {
+        return aleatorio;
+    }
+}
